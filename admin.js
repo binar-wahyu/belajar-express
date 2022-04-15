ebff02c7eb6e019aff10588ca43ad1429feed76b
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-04-15 14:33:30 +0800

    Implementasi morgan middleware

--- admin.js
+++ admin.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  console.log(
+    "Ini Admin Middleware",
+    "Halaman yang akan dituju adalah",
+    req.url
+  );
+  next();
+});
+
 router.get("/admin/info", (req, res) => {
   res.send("From Admin Router - Info Page");
 });
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const adminRouter = require("./admin");
 
 const app = express();
 const port = 3000;
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
 
 app.get("/", (req, res) => {
   res.send("Hi Mom!");
