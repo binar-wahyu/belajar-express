109d921348e4a98bb20c35e8b2b8d6872b0eb016
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-04-15 14:33:29 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  if (req.url === "/order") {
+    return next();
+  }
+
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hi Mom!");
+});
+
+app.get("/product", (req, res) => {
+  res.json(["Macbook Pro", "Asus Vivobook", "Lenovo Yoga"]);
+});
+
+app.get("/order", (req, res) => {
+  res.json([
+    { id: 1, paid: true, userId: 1 },
+    { id: 2, paid: false, userId: 1 },
+    { id: 3, paid: false, userId: 2 },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
