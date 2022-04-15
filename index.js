4ede8a4abf75b144a7f66fde936694925c6e3b5e
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-04-15 14:33:29 +0800

    Implementasi built-in middleware

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
